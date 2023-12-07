0c41b6cb93413f6fb261c7a8326c22a8af859c4c
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-07 16:14:23 +0100

    create-table

---
+++ create-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students
+(
+    StudentId INT PRIMARY KEY NOT NULL,
+    FirstName VARCHAR(80) NOT NULL,
+    LastName VARCHAR(80) NOT NULL,
+    Birthday DATE NOT NULL,
+    City VARCHAR(50) NOT NULL
+)

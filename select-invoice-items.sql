f6831594e3b7a857d21e40180fc59fd0a246932e
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-08 16:54:32 +0100

    select-invoice-items

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -2,4 +2,5 @@ SELECT invoices.InvoiceId, tracks.Name InvoiceItem, tracks.UnitPrice
 FROM invoices
 JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId
 JOIN tracks ON invoice_items.TrackId = tracks.TrackId
-WHERE invoices.InvoiceId = 10;
+WHERE invoices.InvoiceId = 10
+ORDER BY InvoiceItem ASC

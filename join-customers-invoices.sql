7757b4ac05867c99347b8a5a7379cf0f48af7ae4
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-08 15:42:48 +0100

    join-customers-invoices

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -1,4 +1,4 @@
-SELECT customers.FirstName, customers.LastName, customers.Company, invoices.Total InvoicesTotalPrice
+SELECT customers.FirstName, customers.LastName, customers.Company, invoices.Total InvoiceTotalPrice
 FROM customers
 INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
-WHERE customers.Company IS NOT NULL 
+WHERE customers.Company IS NOT NULL

f0b0b7a8d773176e3e8982d44d9f3f9e38006828
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-08 15:22:11 +0100

    first-join

--- first-join.sql
+++ first-join.sql
@@ -1,6 +1,6 @@
 SELECT albums.Title as AlbumsName, artists.Name as ArtistName
 FROM albums
 INNER JOIN artists
-WHERE albums.ArtistId = artists.ArtistId
+WHERE artists.ArtistId = albums.ArtistId
 ORDER BY ArtistName ASC
 LIMIT 100

fa8373e5302c825dc33b2518ab9b2e3109ab9d0f
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-08 18:39:18 +0100

    approximate-age

--- format-date.sql
+++ format-date.sql
@@ -1,3 +1,3 @@
-SELECT employees.FirstName, employees.LastName, CAST(strftime('%Y', employees.HireDate) - strftime('%Y', employees.BirthDate ) AS INT)ApproximateAge
+SELECT employees.FirstName, employees.LastName, CAST(strftime('%Y', employees.HireDate) - strftime('%Y',employees.BirthDate ) AS FLOAT)ApproximateAge
 FROM employees
-ORDER BY ApproximateAge ASC
+ORDER BY ApproximateAge

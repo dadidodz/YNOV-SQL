1e0e65f43eb20becb58f1de6aca643369bfae837
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-08 17:53:33 +0100

    tracks-count

--- tracks-count.sql
+++ tracks-count.sql
@@ -1,4 +1,4 @@
-SELECT genres.Name, COUNT(genres.GenreId)
+SELECT genres.Name, COUNT(genres.GenreId) NumberOfTracks
 FROM genres
 JOIN tracks ON genres.GenreId = tracks.GenreId
 GROUP BY genres.Name, tracks.GenreId

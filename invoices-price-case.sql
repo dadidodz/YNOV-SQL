384b79e5b5e16d7f2616eea3c176bdc71a73ab7e
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-08 19:33:01 +0100

    invoices-price-case

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,7 +1,8 @@
 SELECT invoices.InvoiceId, invoices.Total,
-    CASE
-        WHEN invoices.Total < 5 THEN 'Price lower than 5$'
-        WHEN invoices.Total < 10 THEN 'Price lower than 10$'
-        WHEN invoices.Total > 10 THEN 'Price greater than 10$'
-    END
+       CASE
+           WHEN invoices.Total < 5 THEN 'Price lower than 5$'
+           WHEN invoices.Total < 10 THEN 'Price lower than 10$'
+           ELSE 'Price greater than 10$'
+       END CASE
 FROM 'invoices'
+LIMIT 100

fe655af9058a4bb6be8e8540bb17062f46f8944d
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-19 14:19:19 +0100

    updae-invoices-case

--- update-invoices-case.sql
+++ update-invoices-case.sql
@@ -1,4 +1,4 @@
 UPDATE invoices
-SET Total = CASE WHEN invoices.BillingCountry = 'France' THEN Total = Total + 3
-                WHEN invoices.BillingCountry = 'USA' THEN Total = Total + 5
-                ELSE Total = Total + 1 END
+SET Total = CASE WHEN invoices.BillingCountry = 'France' THEN Total + 3
+                WHEN invoices.BillingCountry = 'USA' THEN Total + 5
+                ELSE Total + 1 END

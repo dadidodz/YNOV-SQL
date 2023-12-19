e7052bd1b9c7ae539ba43ff92c3d482625346b0d
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-19 16:26:57 +0100

    artist-albums-case

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -2,7 +2,7 @@ SELECT Name, count(albums.ArtistId) NbAlbums,
     CASE
         WHEN count(albums.ArtistId) = 1 THEN 'Unproductive'
         WHEN count(albums.ArtistId) > 1 AND count(albums.ArtistId) < 10 THEN 'Productive'
-        ELSE 'Very productive'
+        ELSE 'Very Productive'
     END AS "IsProductive"
 FROM artists
 JOIN albums ON artists.ArtistId = albums.ArtistId

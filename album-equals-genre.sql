6360175fc09104ab1032e46296b598b26839ffe6
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-19 17:11:30 +0100

    albums-equals-genre

--- album-equals-genre.sql
+++ album-equals-genre.sql
@@ -1,11 +1,12 @@
-SELECT albums.AlbumId, artists.ArtistId ,g.Name Title
+SELECT albums.AlbumId, artists.ArtistId, g.Name AS Title
 FROM genres g
 JOIN tracks ON g.GenreId = tracks.GenreId
 JOIN albums ON tracks.AlbumId = albums.AlbumId
 JOIN artists ON albums.ArtistId = artists.ArtistId
-WHERE g.GenreId IN (SELECT g.GenreId
-                    FROM tracks t
-                    WHERE t.TrackId IN (SELECT t.TrackId
-                                        FROM albums al
-                                        WHERE al.Title = g.Name))
-GROUP BY g.Name
+WHERE g.GenreId IN (
+    SELECT g.GenreId
+    FROM tracks t
+    JOIN albums al ON t.AlbumId = al.AlbumId
+    WHERE al.Title = g.Name
+)
+GROUP BY g.Name;

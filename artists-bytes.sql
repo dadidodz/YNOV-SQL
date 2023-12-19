40284f6ab5d0e888374af5f51b8635adfbc15854
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-19 15:58:12 +0100

    artists-bytes

---
+++ artists-bytes.sql
@@ -0,0 +1,5 @@
+SELECT ar.Name, al.Title ,t.Name, round(CAST((t.Bytes / 1000000.0) as Float), 2) as MegaBytes
+FROM tracks t
+JOIN albums al on t.AlbumId = al.AlbumId
+JOIN artists ar on al.ArtistId = ar.ArtistId
+WHERE al.AlbumId = (SELECT al.AlbumId WHERE al.Title = 'American Idiot')

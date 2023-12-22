3f285479d229864ec913f790bed57986e5f1d51a
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-22 17:16:39 +0100

    report-to

--- report-to.sql
+++ report-to.sql
@@ -1,3 +1,8 @@
-SELECT e1.EmployeeId AS EmployeeId, e1.FirstName || ' ' || e1.LastName AS EmployeeName, e2.FirstName || ' ' || e2.LastName AS ReportsTo
+SELECT e1.EmployeeId AS EmployeeId, e1.FirstName || ' ' || e1.LastName AS EmployeeName,
+       CASE
+            WHEN e1.ReportsTo IS NULL THEN NULL
+            ELSE e2.FirstName || ' ' || e2.LastName
+        END AS ReportsTo
 FROM employees e1, employees e2
-WHERE e1.ReportsTo = e2.EmployeeId
+WHERE e1.ReportsTo = e2.EmployeeId OR e1.ReportsTo IS NULL
+GROUP BY e1.EmployeeId

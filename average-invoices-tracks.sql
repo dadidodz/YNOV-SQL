b70efd9bc6c7211ea1978e3ca115bcbd8fa41799
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-09 16:18:36 +0100

    average-invoices-tracks

--- average-invoices-tracks.sql
+++ average-invoices-tracks.sql
@@ -1,4 +1,4 @@
-SELECT i.InvoiceId, round(avg(t.UnitPrice), 2) AS 'Average Price', round(sum((t.Milliseconds / 1000)), 0) AS 'Track Total Time', round((sum(it.UnitPrice)/count(it.TrackId))/sum((t.Milliseconds / 1000)), 5) || '€' AS 'Price by second'
+SELECT i.InvoiceId, round(avg(t.UnitPrice), 2) AS 'Average Price', sum(t.Milliseconds/1000) AS 'Track Total Time', round((sum(it.UnitPrice)/count(it.TrackId))/sum((t.Milliseconds / 1000)), 5) || '€' AS 'Price by second'
 FROM invoices i
 JOIN invoice_items it ON i.InvoiceId = it.InvoiceId
 JOIN tracks t ON it.TrackId = t.TrackId

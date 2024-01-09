a30a62376b3a30151251f69c1f8ae86d29e2ae48
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-09 14:50:46 +0100

    third-best-seller

--- third-best-seller.sql
+++ third-best-seller.sql
@@ -3,9 +3,8 @@ FROM customers c
 JOIN employees e ON c.SupportRepId = e.EmployeeId
 JOIN invoices i ON c.CustomerId = i.CustomerId
 WHERE e.EmployeeId = (SELECT c.SupportRepId
-                        FROM invoices i
-                        JOIN customers c ON i.CustomerId = c.CustomerId
-                        JOIN employees e ON c.SupportRepId = e.EmployeeId
-                        GROUP BY c.SupportRepId
-                        LIMIT 1 OFFSET 2)
+                      FROM invoices i 
+                      JOIN customers c ON i.CustomerId = c.CustomerId
+                      GROUP BY c.SupportRepId
+                      LIMIT 1 OFFSET 2)
 GROUP BY '3rd best seller';

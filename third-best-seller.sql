76098269456eed6cbd23c8d0005654fa2b562bd4
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-09 14:47:01 +0100

    third-best-seller

--- third-best-seller.sql
+++ third-best-seller.sql
@@ -3,11 +3,9 @@ FROM customers c
 JOIN employees e ON c.SupportRepId = e.EmployeeId
 JOIN invoices i ON c.CustomerId = i.CustomerId
 WHERE e.EmployeeId = (SELECT c.SupportRepId
-                      WHERE c.CustomerId = (SELECT i.CustomerId
-                                            WHERE ));
-
-SELECT invoices.InvoiceId, customers.CustomerId, customers.SupportRepId
-FROM invoices
-JOIN customers ON invoices.CustomerId = customers.CustomerId
-JOIN employees ON customers.SupportRepId = employees.EmployeeId
-ORDER BY InvoiceId
+                        FROM invoices i
+                        JOIN customers c ON i.CustomerId = c.CustomerId
+                        JOIN employees e ON c.SupportRepId = e.EmployeeId
+                        GROUP BY c.SupportRepId
+                        LIMIT 1 OFFSET 2)
+GROUP BY '3rd best seller';
